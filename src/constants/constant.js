3c9f0831720461667d6d58ab3a904bd058c287ac
Muhammad Yusuf <90136884+IronyMan05@users.noreply.github.com>
2024-04-29 20:11:34 +0530

    Add files via upload

---
+++ src/constants/constant.js
@@ -0,0 +1,4 @@
+export const logo = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVuNDJjVFk7DX7DBm3j4JlnbHi3R1aKrXUvw&usqp=CAU';
+
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api';
